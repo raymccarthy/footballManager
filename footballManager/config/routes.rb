7c468870df82a685c9ee82a6df11da189aa1161a
Ray McCarthy <ray@cliste.eu>
2012-04-16 21:06:57 +0200

    On the users controller

--- footballManager/config/routes.rb
+++ footballManager/config/routes.rb
@@ -1,7 +1,10 @@
 FootballManager::Application.routes.draw do
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+
 
   root :to => 'pages#home'
 
--- footballManager/spec/controllers/pages_controller_spec.rb
+++ footballManager/spec/controllers/pages_controller_spec.rb
@@ -4,6 +4,11 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+
+  before(:each) do
+    @base_title = "Soccer Manager | "
+
+  end
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
@@ -13,7 +18,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                                    :content => "Home")
+                                    :content => @base_title + "Home")
     end
   end
 
@@ -22,8 +27,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => @base_title + "Contact")
+    end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
 
-
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => @base_title + "About")
+    end
+  end
 end
